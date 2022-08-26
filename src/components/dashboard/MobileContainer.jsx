c2c0ff55eb3c84a5aebf59dd8b36b0f2fdd9a3dc
2hyunkook <hk.lee@raonworks.co.kr>
2022-08-26 11:55:54 +0900

    add pageTitleMobile

--- src/components/dashboard/MobileContainer.jsx
+++ src/components/dashboard/MobileContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Border1pxTiara, Body5 } from "@/styledMixins";
 
-import iconArrowLeft from '@ICONS/icon_arrow_left_black_mobile.png';
+import PageTitle from '@/components/dashboard/PageTitleMobile';
 
 function MobileContainer(props) {
 
@@ -11,12 +11,9 @@ function MobileContainer(props) {
       {
         props.isBorder === true ? (
           <ContentContainer>
-            <HorizontalLine/>
-            <PageTitleContainer>
-              <IconArrowLeft src={iconArrowLeft} /> 
-              <PageTitle>{props.pageTitle}</PageTitle>
-            </PageTitleContainer>
-            <HorizontalLine/>
+              {
+                props.pageTitle !== undefined && (<PageTitle pageTitle={props.pageTitle} handleClick={props.onClickBack} />)
+              }
 
             <WhiteBorderWrapContainer>
               <WhiteBorderContainer>
@@ -33,38 +30,8 @@ function MobileContainer(props) {
   );
 }
 
-const HorizontalLine = styled.div`
-  width: 100%;
-  height: 1px;
-  background-color: var(--mercury);
-`;
-
 const ContentContainer = styled.div`
   width: 100%;
-`;
-
-const PageTitle = styled.div`
-  ${Body5}
-  min-height: 16px;
-  font-weight: 500;
-  color: var(--deep-space-sparkle);
-  line-height: 16px;
-  white-space: nowrap;
-`;
-
-const IconArrowLeft = styled.img`
-  width: 9px;
-  height: 16px;
-  margin-right: 10px;
-`;
-
-const PageTitleContainer = styled.div`
-  width: 100%;
-  height: 40px;
-  padding: 0 16px;
-  background-color: var(--white);
-  display: flex;
-  align-items: center;
 `;
 
 const Container = styled.div`
