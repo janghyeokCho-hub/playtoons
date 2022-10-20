2239dc4e7a2d0c70bfe16a19eef91a71c67d6579
JangHyeok-Raon <jh.cho@raonworks.co.kr>
2022-10-20 21:35:14 +0900

    타입 이름으로 변경

--- src/components/landingPage/Curation.jsx
+++ src/components/landingPage/Curation.jsx
@@ -8,9 +8,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/pro-solid-svg-icons";
 
 const Curation = ({ item }) => {
-  console.log("item : ", item);
   const userInfo = useSelector(({ login }) => login.userInfo);
   const thumbnailImgURL = useFilePath(item?.thumbnailImage);
+
   return (
     <Link to={`/post/detail/${item?.type?.code}/${item?.id}`}>
       <div className="cx_thumb">
@@ -23,9 +23,7 @@ const Curation = ({ item }) => {
         </p>
       </div>
       <div className="cx_txt">
-        <p className="t1 c-blue">
-          {item.categoryId}카테고리 아이디만 넘어오고 있음 따로 API 없는듯
-        </p>
+        <p className="t1 c-blue">{item?.type?.name}</p>
         <p className="h1">{item.title}</p>
         <p className="t1">{userInfo?.name}</p>
         <p className="t1">{item.endAt || 0}話</p>
