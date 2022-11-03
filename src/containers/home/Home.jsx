0f6d6816006580cec6c576c442bfcbd8ddf92010
JangHyeok-Raon <jh.cho@raonworks.co.kr>
2022-11-03 20:47:06 +0900

    홈 퍼블 적용

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useCallback } from "react";
+import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { setDim } from "@/modules/redux/ducks/dim";
 import { setContainer } from "@/modules/redux/ducks/container";
@@ -7,7 +8,7 @@ import BannerItems from "@COMPONENTS/home/BannerItems";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/pro-solid-svg-icons";
-import BestWebtoonItems from "@/components/home/BestWebtoonItems";
+import BestWebtoonItems from "@/components/home/webtoon/BestWebtoonItems";
 
 const Home = (props) => {
   const dispatch = useDispatch();
@@ -143,8 +144,243 @@ const Home = (props) => {
           </div>
         </div>
       </div>
+
+      <div className="main_area">
+        <div className="inr-c">
+          <div className="hd_titbox">
+            <h2 className="m_tit1">📝ウェブ小説</h2>
+            <a href="#" className="rgh btn-pk n blue2">
+              <span className="ico_arr_link">
+                すべてみる
+                <FontAwesomeIcon icon={faAngleRight} />
+              </span>
+            </a>
+          </div>
+
+          <div className="lst_comic1 long">
+            <CurationItems curationNum={2} />
+          </div>
+        </div>
+      </div>
+
+      <div className="main_area">
+        <div className="inr-c">
+          <div className="hd_titbox">
+            <h2 className="m_tit1">📷写真集</h2>
+            <a href="#" className="rgh btn-pk n blue2">
+              <span className="ico_arr_link">
+                すべてみる
+                <FontAwesomeIcon icon={faAngleRight} />
+              </span>
+            </a>
+          </div>
+
+          <div className="lst_comic1 long">
+            <CurationItems curationNum={2} />
+          </div>
+        </div>
+      </div>
+
+      <div className="main_area">
+        <div className="inr-c">
+          <div className="hd_titbox">
+            <h2 className="m_tit1">📝一押しウェブ小説</h2>
+            <a href="#" className="rgh btn-pk n blue2 view-m">
+              <span className="ico_arr_link">
+                すべてみる <i className="fa-solid fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+
+          <div className="lst_novel1">
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className="col">
+              <a href="#">
+                <div className="thumb">
+                  <ImgBgSpan
+                    bgImg={require("@IMAGES/tmp_novel_s.jpg")}
+                  ></ImgBgSpan>
+                </div>
+                <div className="txt">
+                  <p className="h1">戦略的ドリンク注文 </p>
+                  <p className="t1">
+                    ショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまいショートショート3作目。
+                    朝から電車で乃木坂駅まで移動したさくちゃん。
+                    マスクと帽子で顔を隠していたつもりが、
+                    ほんのわずかな油断で人だかりが出来てしまい
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main_area mb1">
+        <div className="ad_longad1">
+          <div className="inr-c">
+            <div className="imgs">
+              <ImgBgSpan bgImg={require("@IMAGES/longad_img.png")}></ImgBgSpan>
+            </div>
+            <div className="txt">
+              <p className="h1">クリエイターになる</p>
+              <p className="t1">自分の作品を世界に届けよう！</p>
+              <a href="#" className="btn-pk bdrs">
+                詳しくみる
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main_area">
+        <div className="inr-c">
+          <div className="hd_titbox">
+            <h2 className="m_tit1">完結ウェブトゥーン</h2>
+            <a href="#" className="rgh btn-pk n blue2">
+              <span className="ico_arr_link">
+                すべてみる
+                <FontAwesomeIcon icon={faAngleRight} />
+              </span>
+            </a>
+          </div>
+
+          <div className="lst_comic1 long">
+            <CurationItems curationNum={2} />
+          </div>
+        </div>
+      </div>
+
+      <div className="main_area">
+        <div className="inr-c">
+          <div className="hd_titbox">
+            <h2 className="m_tit1">連載中のウェブトゥーン</h2>
+            <a href="#" className="rgh btn-pk n blue2">
+              <span className="ico_arr_link">
+                すべてみる
+                <FontAwesomeIcon icon={faAngleRight} />
+              </span>
+            </a>
+          </div>
+
+          <div className="lst_comic1 long">
+            <CurationItems curationNum={2} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+const ImgBgSpan = styled.span`
+  background-image: url(${(props) => props.bgImg});
+`;
 
 export default Home;
