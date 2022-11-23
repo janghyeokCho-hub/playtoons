6ee70f7951e616b819e2b5b0a358666a2698bb05
JangHyeok-Raon <jh.cho@raonworks.co.kr>
2022-11-24 00:08:23 +0900

    Timeline Swiper 적용중

--- src/components/timeline/TimelineItem.jsx
+++ src/components/timeline/TimelineItem.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faCommentQuote,
+  faHeart,
+  faShare,
+} from "@fortawesome/pro-solid-svg-icons";
+import { faEllipsisVertical } from "@fortawesome/pro-regular-svg-icons";
+import { faPenToSquare, faTrash } from "@fortawesome/pro-light-svg-icons";
+import useFilePath from "@/hook/useFilePath";
 
-const TimelineItem = () => {
-  return <></>;
+const TimelineItem = ({ item }) => {
+  const [isControlShow, setIsControlShow] = useState(false);
+  const activeThumbnailImage = useFilePath(item?.thumbnailImage);
+  const activeProfileImage = useFilePath(item?.author?.profileImage);
+
+  return (
+    <>
+      <div className="col active">
+        <div className="bt_bar">
+          <div className="bar">
+            <span style={{ width: "20%" }}></span>
+          </div>
+        </div>
+        <div className="thumb">
+          <ImgSpan bgImg={activeThumbnailImage}></ImgSpan>
+        </div>
+        <div className="cont">
+          <p className="t1">{item?.title}</p>
+          <div className="t_profile">
+            <ImgSpan className="im" bgImg={activeProfileImage}></ImgSpan>
+            <p>{item?.nickname}</p>
+          </div>
+          <button type="button" className="btn-pk n blue">
+            フォロー
+          </button>
+        </div>
+
+        <div className="bt_top">
+          <button type="button" className="btn01 view-m">
+            <span className="i">
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </span>
+            <span className="hidden">prev</span>
+          </button>
+          <button
+            type="button"
+            className="btn01"
+            onClick={() => setIsControlShow(!isControlShow)}
+          >
+            <span className="i">
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </span>
+          </button>
+          {isControlShow && (
+            <div className="box_drop">
+              <ul>
+                <li>
+                  <a href="#" onclick="showComm(this); return false;">
+                    <FontAwesomeIcon icon={faPenToSquare} />
+                    修正
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onclick="showDrop('popDelete'); return false;">
+                    <FontAwesomeIcon icon={faTrash} />
+                    削除
+                  </a>
+                </li>
+                {/*<!-- <li><a href="#" onclick="showDrop('popReport'); return false;"><i className="fa-light fa-flag"></i>通報</a></li> -->*/}
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="bt_botm">
+          <button type="button" className="btn01">
+            <span className="i">
+              <FontAwesomeIcon icon={faHeart} />
+            </span>
+            <span>{item?.likeCount}</span>
+          </button>
+          <button type="button" className="btn01">
+            <span className="i">
+              <FontAwesomeIcon icon={faCommentQuote} />
+            </span>
+            <span>{item?.viewCount}</span>
+          </button>
+          <button type="button" className="btn01">
+            <span className="i">
+              <FontAwesomeIcon icon={faShare} />
+            </span>
+            <span className="hidden">share</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
+
+const ImgSpan = styled.span`
+  background-image: url(${(props) => props.bgImg});
+`;
 
 export default TimelineItem;
--- src/components/timeline/TimelineItems.jsx
+++ src/components/timeline/TimelineItems.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useRef, useMemo } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/pro-solid-svg-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import TimelineItem from "./TimelineItem";
 
-const TimelineItems = () => {
-  return <></>;
+const TimelineItems = ({ items }) => {
+  SwiperCore.use([Navigation]);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  const renderItems = useMemo(() => {
+    return (
+      <div className="swiper-wrapper">
+        {items.map((item, index) => {
+          return (
+            <SwiperSlide
+              key={`recent_${index}`}
+              className="swiper-slide"
+              virtualIndex={index}
+            >
+              <TimelineItem item={item} />
+            </SwiperSlide>
+          );
+        })}
+      </div>
+    );
+  }, [items]);
+
+  return (
+    <>
+      <div className="rgh">
+        <button ref={prevRef} type="button" className="btn01 slide_st">
+          <span className="i">
+            <FontAwesomeIcon icon={faChevronUp} />
+          </span>
+        </button>
+        <button ref={nextRef} type="button" className="btn01 slide_st">
+          <span className="i">
+            <FontAwesomeIcon icon={faChevronDown} />
+          </span>
+        </button>
+      </div>
+
+      <Swiper
+        className="swiper-container-vertical"
+        spaceBetween={50}
+        slidesPerView="auto"
+        direction="vertical"
+        navigation={{
+          prevEl: prevRef?.current,
+          nextEl: nextRef?.current,
+        }}
+      >
+        {renderItems}
+      </Swiper>
+    </>
+  );
 };
 
 export default TimelineItems;
