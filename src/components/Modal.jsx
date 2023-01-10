47295d79e729f22ada76694a870adc235700ab58
2hyunkook <hk.lee@raonworks.co.kr>
2023-01-10 09:21:44 +0900

    modal animation 적용 버그 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -57,9 +57,11 @@ export default forwardRef( function Modal(props) {
   
 
   useEffect(() => {
-    document.body.style= `overflow: hidden`;
-    window.addEventListener("beforeunload", (e) => handleClose());     //창닫기, 주소창을 통항 이동시 
-    window.addEventListener("keydown", (e) => handleKeydown(e));       //키 이벤트작동시
+    if( show ){
+      document.body.style= `overflow: hidden`;
+      window.addEventListener("beforeunload", (e) => handleClose());     //창닫기, 주소창을 통항 이동시 
+      window.addEventListener("keydown", (e) => handleKeydown(e));       //키 이벤트작동시
+    }
     
     return () => {
       handleClose();
