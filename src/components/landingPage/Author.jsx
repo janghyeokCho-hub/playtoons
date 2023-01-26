3cd84592efd4959b19b3eae545daaa61512a48c8
2hyunkook <hk.lee@raonworks.co.kr>
2023-01-26 13:58:08 +0900

    home, landing page like 변경

--- src/components/landingPage/Author.jsx
+++ src/components/landingPage/Author.jsx
@@ -1,9 +1,9 @@
-import React, { useCallback } from "react";
-import styled from "styled-components";
 import useFilePath from "@/hook/useFilePath";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { currentAuthorInit } from "@/modules/redux/ducks/author";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import ImageBackground from "../dashboard/ImageBackground";
 
 const Author = ({ item }) => {
   const dispatch = useDispatch();
@@ -25,14 +25,15 @@ const Author = ({ item }) => {
         onClick={handleCurrentAuthorInit}
       >
         {!backgroundImgLoading && (
-          <ImgTmpProfileBgDiv
+          <ImageBackground
+            type={"div"}
             className="pf_thumb"
-            bgImg={backgroundImgURL}
-          ></ImgTmpProfileBgDiv>
+            hash={item?.backgroundImage}
+          ></ImageBackground>
         )}
         <div className="pf_txt">
           <div className="icon">
-            {!profileImgLoading && <img src={profileImgURL} alt="profile" />}
+            {!profileImgLoading && <img src={profileImgURL} alt="" />}
           </div>
           <p className="h1">{item.nickname}</p>
           <p className="t1">{item.description}</p>
@@ -42,12 +43,5 @@ const Author = ({ item }) => {
   );
 };
 
-const ImgTmpProfileBgDiv = styled.div`
-  background-image: url(${(props) => props.bgImg});
-  height: 80px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-`;
 
 export default Author;
